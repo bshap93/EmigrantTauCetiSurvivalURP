d167e9398a73d75d91b7fcdeb2d880ea1c757173
Ben Shapiro <bshap93@gmail.com>
2024-09-19 12:26:05 -0600

    Minor tweak

--- Assets/Combat/Weapons/Scripts/LaserTool.cs
+++ Assets/Combat/Weapons/Scripts/LaserTool.cs
@@ -1,4 +1,5 @@
-﻿using Characters.Enemies;
+﻿using System.Collections.Generic;
+using Characters.Enemies;
 using Combat.Attacks.Commands;
 using UnityEngine;
 
@@ -6,18 +7,33 @@ namespace Combat.Weapons.Scripts
 {
     public class LaserTool : Weapon
     {
+        public LineRenderer lineRenderer;
         public Transform firePoint;
+        public float laserWidth = 0.1f;
+        public float laserRange = 10f;
 
         void Start()
         {
             damage = 10f;
-            range = 50f;
+            range = laserRange;
             AttackCommand = new RangedAttackCommand(this, range, firePoint);
         }
 
         public override void Attack(Enemy target)
         {
             Debug.Log("LaserTool: Attack");
+            FireLaserTool();
+        }
+        
+        void FireLaserTool()
+        {
+
+        }
+        
+        IEnumerator<WaitForSeconds> LaserEffect()
+        {
+            
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
