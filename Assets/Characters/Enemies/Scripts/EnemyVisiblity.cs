7257c44c63686f7c0aed27ee06fb086d039409c8
Ben Shapiro <bshap93@gmail.com>
2024-09-19 11:03:43 -0600

    Add weapon and player to scene

--- Assets/Characters/Enemies/Scripts/EnemyVisiblity.cs
+++ Assets/Characters/Enemies/Scripts/EnemyVisiblity.cs
@@ -1,5 +1,4 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Characters.Enemies.Scripts
 {
@@ -90,73 +89,4 @@ namespace Characters.Enemies.Scripts
             return canSee;
         }
     }
-
-#if UNITY_EDITOR
-// A custom editor for the EnemyVisibility class. Visualises and allows
-// editing the visible range.
-    [CustomEditor(typeof(EnemyVisiblity))]
-    public class EnemyVisibilityEditor : Editor
-    {
-        // Called when Unity needs to draw the Scene view. 
-        void OnSceneGUI()
-        {
-            // Get a reference to the EnemyVisibility script we're looking at
-            var visibility = target as EnemyVisiblity;
-
-            // Start drawing at 10% opacity
-            Handles.color = new Color(1, 1, 1, 0.1f);
-
-            // Drawing an arc sweeps from the point you give it. We want to
-            // draw the arc such that the middle of the arc is in front of the
-            // object, so we'll take the forward direction and rotate it by
-            // half the angle.
-
-            if (visibility != null)
-            {
-                var forwardPointMinusHalfAngle =
-                    // rotate around the Y axis by half the angle
-                    Quaternion.Euler(0, -visibility.angle / 2, 0)
-                    // rotate the forward direction by this
-                    * visibility.transform.forward;
-
-                // Draw the arc to visualise the visibility arc
-                var arcStart =
-                    forwardPointMinusHalfAngle * visibility.maxDistance;
-
-                Handles.DrawSolidArc(
-                    visibility.transform.position, // The center of the arc
-                    Vector3.up, // The up-direction of the arc
-                    arcStart, // The point where it begins
-                    visibility.angle, // The angle of the arc
-                    visibility.maxDistance // The radius of the arc
-                );
-            }
-
-
-            // Draw a scale handle at the edge of the arc; if the user drags
-            // it, update the arc size.
-
-            // Reset the handle colour to full opacity
-            Handles.color = Color.white;
-
-            // Compute the position of the handle, based on the object's
-            // position, the direction it's facing, and the distance
-            var handlePosition =
-                visibility.transform.position +
-                visibility.transform.forward * visibility.maxDistance;
-
-            // Draw the handle, and store its result.
-            visibility.maxDistance = Handles.ScaleValueHandle(
-                visibility.maxDistance, // current value
-                handlePosition, // handle position
-                visibility.transform.rotation, // orientation
-                1, // size
-                Handles.ConeHandleCap, // cap to draw
-                0.25f); // snap to multiples of this 
-            // if the snapping key is
-            // held down
-        }
-    }
-#endif
-// END enemy_visibility
 }
