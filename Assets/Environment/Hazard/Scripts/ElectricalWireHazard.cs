2f43fd93bc98ce15d64c58aeb2683b098beb3445
Ben Shapiro <bshap93@gmail.com>
2024-09-26 14:23:59 -0600

    Add electrical wires

---
+++ Assets/Environment/Hazard/Scripts/ElectricalWireHazard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Environment.Hazard.Scripts
+{
+    public class ElectricalWireHazard : MonoBehaviour
+    {
+        public ParticleSystem sparksParticleSystem;
+        
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                var playerController = other.GetComponent<CharacterController>();
+                if (playerController == null) return;
+
+                Debug.Log("Player entered electrical wire trigger");
+
+                sparksParticleSystem.Play();
+            }
+        }
+    }
+}
